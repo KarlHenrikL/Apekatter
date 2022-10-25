a4e811954c78c33102690fa687e226d046cca427
klerm <lermokarl@gmail.com>
2022-10-25 14:26:27 +0200

    init

---
+++ øving 9.py
@@ -0,0 +1,15 @@
+
+
+#d) Lag en klasse for en avtale. En avtale skal minimum ha:
+#a. En tittel som sier hva denne avtalen er (streng)
+#b. Et sted (streng)
+#c. Et starttidspunkt (datetime objekt, se hint nederst)
+#d. En varighet i minutter (int)
+
+
+class Avtale: #a
+    def __init__(self, tittel,sted, starttidspunkt=0, varighet=0):
+        self.tittel = tittel
+        self.sted = sted
+        self.starttidspunkt = starttidspunkt
+        self.varighet = varighet
