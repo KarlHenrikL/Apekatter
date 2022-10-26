78debef2f0d5c1844edb213425bbd10f9076f94c
klerm <lermokarl@gmail.com>
2022-10-26 10:34:38 +0200

    init

--- øving 9.py
+++ øving 9.py
@@ -5,11 +5,30 @@
 #b. Et sted (streng)
 #c. Et starttidspunkt (datetime objekt, se hint nederst)
 #d. En varighet i minutter (int)
+import (datetime)
 
-
-class Avtale: #a
+class Avtale: #d
     def __init__(self, tittel,sted, starttidspunkt=0, varighet=0):
         self.tittel = tittel
         self.sted = sted
         self.starttidspunkt = starttidspunkt
         self.varighet = varighet
+
+#e) Lag en __str__ metode for avtaler som returnerer en streng som kan skrives ut med en print-
+#setning slik at du får skrevet ut avtalen med alle egenskapene til avtalen på et leselig format
+#for brukeren.
+
+    def __str__(self):  # e
+        return f"tittel :{self.tittel}, sted: {self.sted}, starttidspunkt: {self.starttidspunkt},varighet: {varighet} "
+
+#f) Lag en funksjon som lar brukeren skrive inn en ny avtale. Funksjonen skal bruke input-
+#funksjonen til å lese inn egenskapene til avtalen og skal sjekke at det brukeren skriver er
+#gyldig, for eksempel at varighet er et tall. Funksjonen skal returnere et avtale-objekt
+
+    def ny_avtale (self): #f
+        while True:
+            try:
+                avtale = float(input("skriv inn retning 1:"))
+                break
+            except:
+                print("ikke gyldig input prøv på nytt")
