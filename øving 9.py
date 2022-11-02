aa837cbac89b844d3db5af322350659ea501435d
SondreC00 <116153180+SondreC00@users.noreply.github.com>
2022-11-02 09:59:42 +0100

    Update øving 9.py

--- øving 9.py
+++ øving 9.py
@@ -1,84 +1,58 @@
-#sondre er gay
+#Astror e gay
 
-import (datetime)
+from datetime import date,time
 
-liste= []
-
-class Avtale: #a
-    def __init__(self, tittel,sted, starttidspunkt=0, varighet=0):
-        self.tittel = tittel
-        self.sted = sted
-        self.starttidspunkt = starttidspunkt
-        self.varighet = varighet
-
-    def hovedmeny(start):
-        while start == 1:
-            print("1: Les inn avtaler fra fil")
-            print("2: Skriv avtalene til fil")
-            print("3: Skriv inn en ny avtale")
-            print("4: Skriv ut alle avtalene")
-            print("5: Slett en avtale")
-            print("6: Rediger en avtale")
-            print("0: Avslutt")
-            valg = int(input("velg et alternativ: "))
-            if valg == 1:
-                avtaler_fra_fil()
-            elif valg == 2:
-                avtaler_til_fil()
-            elif valg == 3:
-                ny_avtale()
-            elif valg == 4:
-                skriv_ut_alle_avtaler()
-            elif valg == 5:
-                slette_avtale()
-            elif valg == 6:
-                redigere_avtale()
-            elif valg == 7:
-                exit
-            else:
-                print("Ugyldig svar, vennligst bruk 1-6")
-        return hovedmeny(1)
+class Avtale:
+    def __init__(self,tittel, sted,dato,tid,varighet,personer):
+        self.tittel=tittel
+        self.sted=sted
+        self.tid=tid
+        self.dato=dato
+        self.varighet=varighet
+        self.personer=personer
     def __str__(self):
-        return f"Navn :{self.tittel}, Sted: {self.sted}, Starttidspunkt: {self.starttidspunkt}, Varighet: {self.varighet}, tema {self.tema}"
+        return  self.tittel.upper()+": "+str(self.dato)+", kl "+str(self.tid)+" skal "+self.personerStreng()+" møte på "+self.sted+". Planlagt tid: "+str(self.varighet)+" minutter."
+    
+    def personerStreng(self):
+        result=self.personer[0]
+        for i in range(1,len(self.personer)-1,1):
+            result+=", "+self.personer[i]
+        if(len(self.personer)>1): result+=" og "+self.personer[-1]
+        return result
 
+def skrivAvtaleListe(avtaler,overskrift=""):
+    if(not overskrift==""): print("_____"+overskrift+"_____")
+    for i in range(len(avtaler)):
+        print(f"\t{i}   {avtaler[i].tittel}" )
 
-    def ny_avtale (self): #f
-       while True:
-            try:
-                tittel = input("skriv inn ny tittel:"))
+#region get input functions
+def tryint(prompt):
+    try:
+        return int(input(prompt))
+    except:
+        print("Verdien du skrev inn kunne ikke leses som et tall.")
+        if(input("Prøv igjen? [y/n]: ").lower()[0]=="y"): return tryint(prompt)
+        return 0
+def trydate(prompt):
+    try:
+        args=input(prompt).split('-')
+        return date(int(args[0]),int(args[1]),int(args[2]))
+    except:
+        print("Verdien du skrev inn kunne ikke leses som en dato.")
+        if(input("Prøv igjen? [y/n]: ").lower()[0]=="y"): return trydate(prompt)
+        return 0
+    
+def trytime(prompt):
+    try:
+        args=input(prompt).split(':')
+        return time(int(args[0]),int(args[1]))
+    except:
+        print("Verdien du skrev inn kunne ikke leses som en dato.")
+        if(input("Prøv igjen? [y/n]: ").lower()[0]=="y"): return trydate(prompt)
+        return 0
+#endregion
 
-                sted = input("skriv inn ny sted:"))
-
-                starttidspunkt = int(input("skriv inn ny starttidspunkt:"))
-
-                varighet = int(input("skriv inn ny varighet:"))
-
-                tema = input("skriv inn ny tema:"))
-
-            except:
-                print("ikke gyldig avtale prøv på nytt")
-
-            else:
-                break
-
-
-        return ny_avtale
-if __name__ == "__main__":
-
-#g) Lag en funksjon som skriver ut ei liste med avtaler til skjermen. Funksjonen skal minimum
-#skrive ut indeksen til avtalen i lista og tittel til avtalen. Den kan alternativt skrive ut indeksen
-#til avtalen og så hele avtalen som definert i __str__ metoden. Funksjonen skal ha en frivillig
-#parameter «overskrift» som skal være en overskrift som funksjonen skriver ut før avtalene i
-#lista. Funksjonen skal inkludere indeksen til hver avtale i utskriften.
-
-    def listemedavtaler(liste, overskrift "avtaler"): #g
-        for i, value in enumerate(liste):
-            print (i, value)
-    return liste
-
-
-#h)Lag en funksjon som lagrer ei liste med avtaler til ei tekstfil. Tenk over hva som vil være et 
-#fornuftig format for ei slik tekstfil.
+#region Create/edit h-j)
 def _lagAvtale():
     avtaleListe.append(lagAvtale())
 
@@ -86,9 +60,6 @@ def lagAvtale():
     print("Du skriver nå en ny avtale:")
     return Avtale(input("Tittel: "),input("Sted: "),trydate("Dato (yyyy-mm-dd): "), trytime("Tid (hh:mm): "),tryint("Varighet (minutter): "),input('Personer: ').split(','))
 
-#append() insert() variabel.extend(det man vil legge til).
-#i)Lag en funksjon som leser inn ei liste med avtaler fra ei tekstfil på samme format som dere 
-#definerte for funksjonen som skriver fila med avtaler. 
 def velgAvtale():
     if(len(avtaleListe)==0): return -1
     try:
@@ -100,12 +71,7 @@ def velgAvtale():
     if(i>=len(avtaleListe)):
         return len(avtaleListe)-1
     return i
-
-#j)Lag en funksjon som tar inn ei liste med avtaler og en dato og returnerer ei liste med alle 
-#avtalene som foregår på denne datoen. Funksjonen trenger bare å sjekke om datoen 
-#stemmer med dato-delen av starttidspunktet til avtalen. 
-
-def _endreAvtale(i=-1): #n
+def _endreAvtale(i=-1):
     if i==-1: i=velgAvtale()
     if i==-1: return
     p=[
@@ -135,3 +101,94 @@ def _endreAvtale(i=-1): #n
         avtaleListe[i].personer=input("Nye personer: ").split(',')
     
     if not j==6: _endreAvtale(i)
+
+def _slettAvtale():
+    i=velgAvtale()
+    if i==-1: return
+    print(f"Du har valgt avtale {i}:\n\t{avtaleListe[i]}")
+    if input("Ønsker du å slette? [y/n]: ").lower[0]=="y":
+        del avtaleListe[i]
+
+def _avtaleDetaljer():
+    i=velgAvtale()
+    if i==-1: return
+    print(f"Avtale {i}:\n\t{avtaleListe[i]}")
+#endregion
+
+#region read/write file
+def _lagreAvtaleListe():
+    lagreAvtaleListe(avtaleListe,input("Destinasjonsfil: "))
+def lagreAvtaleListe(avtaler,destinasjonsfil="mine avtaler.txt"):
+    result=""
+    for a in avtaler:
+        for p in [a.tittel, a.sted,str(a.dato),str(a.tid),str(a.varighet)]: result+=p+","
+        for person in a.personer: result+=person+","
+        result=result[:-1]+"\n"
+    f = open(destinasjonsfil, "w")
+    f.write(result)
+    f.close()
+def _lesAvtaleListeFraFil():
+    for avtale in lesAvtaleListeFraFil(input("Les fra fil: ")):
+        avtaleListe.append(avtale)
+def lesAvtaleListeFraFil(fil):
+    f = open(fil, "r")
+    tekst=f.read()
+    f.close()
+    avtaler=[]
+    for avtale in tekst.split('\n'):
+        args=avtale.split(',')
+        if(len(args)<4): continue
+        dato=args[2].split('-')
+        tid=args[3].split(':')
+        personer=args[:4]
+        avtaler.append(Avtale(args[0],args[1],date(int(dato[0]),int(dato[1]),int(dato[2])),
+                              time(int(tid[0]),int(tid[1]),int(tid[2])),int(args[4]),personer))
+    return avtaler
+#endregion
+
+#region search functions
+def avtalerGittDato(avtaleListe,dato):
+    return søkAvtaler(avtaleListe,lambda avtale:date==avtale.date)
+def avtalerGittTittel(avtaleListe,tittel):
+    return søkAvtaler(avtaleListe,lambda avtale:tittel in avtale.tittel)
+def _søkAvtaler():
+    a=input("Søkeord: ")
+    kriterie=lambda avtale:a in (avtale.tittel+avtale.sted)
+    resultater=søkAvtaler(avtaleListe,kriterie)
+    skrivAvtaleListe(resultater,f"Ditt søk ga {len(resultater)} resultater")
+def søkAvtaler(avtaleListe,condition):
+    result=[]
+    for avtale in avtaleListe:
+        if condition(avtale): result.append(avtale)
+    return result
+#endregion
+
+def meny():
+    print(f"Avtaleboka mi: ({len(avtaleListe)})")
+    if(len(avtaleListe)==0): print("\tDu har ikke lagd noen avtaler enda.")
+    else: skrivAvtaleListe(avtaleListe)
+    handlinger=["Lag ny avtale",
+                "Les inn avtaler fra fil",
+                "Skriv avtaler til fil",
+                "Søk i avtaler",
+                "Avslutt"]
+    funksjoner=[_lagAvtale,_lesAvtaleListeFraFil,_lagreAvtaleListe,_søkAvtaler,lambda:None]
+    
+    #Hvis vi ikke har noen avtaler enda, er det ikke vits å vise disse handlingene til brukeren.
+    if(len(avtaleListe)>0):
+        handlinger.insert(1,"Endre avtale")
+        handlinger.insert(2,"Slett avtale")
+        handlinger.insert(3,"Se avtaledetaljer")
+        funksjoner.insert(1,_endreAvtale)
+        funksjoner.insert(2,_slettAvtale)
+        funksjoner.insert(3,_avtaleDetaljer)
+    
+    for i in range(len(handlinger)):
+        print(f"{str(i+1)}: {handlinger[i]}")
+    i=int(input(f"Velg en handling [1-{len(handlinger)}]: "))-1
+    funksjoner[i]()
+    if(not handlinger[i]=="Avslutt"): meny()
+
+avtaleListe=[]
+if __name__=="__main__":
+    meny()
